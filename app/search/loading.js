e82c0228cef7214dc0572f6b4cffdd59a96c947d
ChitranshAgrawal <chitranshaga.2345@gmail.com>
2025-06-24 11:29:34 +0530

    added loading for all pages

---
+++ app/search/loading.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="h-screen w-screen flex mt-20 justify-center">
+      <div
+        className="animate-spin rounded-full border-t-2 border-b-2 border-red-600 border-solid"
+        style={{
+          height: "4rem",
+          width: "4rem",
+          borderLeftColor: "transparent",
+          borderRightColor: "transparent",
+        }}
+      />
+    </div>
+  );
+};
+
+export default loading;
+
