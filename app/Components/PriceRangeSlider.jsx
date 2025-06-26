ad68953d4476f72e35545679f36e755f3493af4a
ChitranshAgrawal <chitranshaga.2345@gmail.com>
2025-06-26 17:59:33 +0530

    message

--- app/Components/PriceRangeSlider.jsx
+++ app/Components/PriceRangeSlider.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 
 const PriceRangeSlider = ({ minBudget, maxBudget, onRangeChange }) => {
--- app/api/about/career/route.js
+++ app/api/about/career/route.js
@@ -6,7 +6,8 @@ const { Career } = models;
 
 export async function GET() {
   try {
-    const career = await Career.find();
+    const career = await Career.find({active: true});
+    console.log("Career listings fetched successfully:", career);
     return NextResponse.json({ success: true, career }, { status: 200 });
   } catch (error) {
     console.error("Error fetching career listings:", error);
